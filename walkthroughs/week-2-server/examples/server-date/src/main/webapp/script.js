1f76ef8822a3ade23acd07887e5467a70857446f
MarioJim <mario.emilio.j@gmail.com>
2021-03-27 19:28:50 -0600

    Remove walkthroughs and move project to root

--- walkthroughs/week-2-server/examples/server-date/src/main/webapp/script.js
+++
@@ -1,8 +0,0 @@
-/** Fetches the current date from the server and adds it to the page. */
-async function showServerTime() {
-  const responseFromServer = await fetch('/date');
-  const textFromResponse = await responseFromServer.text();
-
-  const dateContainer = document.getElementById('date-container');
-  dateContainer.innerText = textFromResponse;
-}
