ac42af54c205797489decfc3ada820b4d52b18d9
Diego Gomez Cota <gomezcotadiego@gmail.com>
2021-04-02 06:45:42 +0000

    Add pages resulting from event search

---
+++ src/main/webapp/user-data.js
@@ -0,0 +1,15 @@
+/** Fetches the user ID created from the server. */
+async function showUserData() {
+    // parse URL to get data
+    const url = new URL(window.location.href);
+    const user = url.searchParams.get('user');
+    const room = url.searchParams.get('room');
+    // Add userID to user page
+    const userDataContainer = document.getElementById('user-ID-container');
+    var headerUser = document.createElement('h2');
+    var headerRoom = document.createElement('h2');
+    headerUser.append(user);
+    headerRoom.append(room);
+    userDataContainer.appendChild(headerUser);
+    userDataContainer.appendChild(headerRoom);
+}
