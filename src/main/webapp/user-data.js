8bcc06e84e40a441049f89fe51f811abdaf35b13
Diego Gomez Cota <gomezcotadiego@gmail.com>
2021-04-09 04:12:59 +0000

    Add redirection from calendar button to calendar page

--- src/main/webapp/user-data.js
+++ src/main/webapp/user-data.js
@@ -1,15 +1,18 @@
-/** Fetches the user ID created from the server. */
-async function showUserData() {
+// function changeURL() {
+//     var theURL = window.location.pathname;
+//     return  theURL.replace("/url_part_to_change/", "/new_url_part/");
+// }
+
+/** Fetches the user data from URL and sends it to calendar page. */
+function redirectToCalendarPage() {
     // parse URL to get data
     const url = new URL(window.location.href);
     const user = url.searchParams.get('user');
-    const room = url.searchParams.get('room');
-    // Add userID to user page
-    const userDataContainer = document.getElementById('user-ID-container');
-    var headerUser = document.createElement('h2');
-    var headerRoom = document.createElement('h2');
-    headerUser.append("User ID: " + user);
-    headerRoom.append("Room ID: " + room);
-    userDataContainer.appendChild(headerUser);
-    userDataContainer.appendChild(headerRoom);
+    const event = url.searchParams.get('event');
+    // Send user to calendar page
+    const params = new URLSearchParams({
+        event: event,
+        user: user
+    });
+    window.location.replace("calendar.html?" + params.toString())
 }
