e7b1eb0fe278f49e7870d856688aa6b329ed993e
Diego Gomez Cota <gomezcotadiego@gmail.com>
2021-04-09 17:47:38 +0000

    Add redirection from user page to location page

--- src/main/webapp/user-data.js
+++ src/main/webapp/user-data.js
@@ -1,10 +1,4 @@
-// function changeURL() {
-//     var theURL = window.location.pathname;
-//     return  theURL.replace("/url_part_to_change/", "/new_url_part/");
-// }
-
-/** Fetches the user data from URL and sends it to calendar page. */
-function redirectToCalendarPage() {
+function getUserData() {
     // parse URL to get data
     const url = new URL(window.location.href);
     const user = url.searchParams.get('user');
@@ -14,5 +8,15 @@ function redirectToCalendarPage() {
         event: event,
         user: user
     });
-    window.location.replace("calendar.html?" + params.toString())
+    return params.toString();
+}
+
+/** Fetches the user data from URL and sends it to calendar page. */
+function redirectToCalendarPage() {
+    window.location.replace("calendar.html?" + getUserData());
+}
+
+/** Fetches the user data from URL and sends it to location page. */
+function redirectToLocationPage() {
+    window.location.replace("choose-loc.html?" + getUserData());
 }
