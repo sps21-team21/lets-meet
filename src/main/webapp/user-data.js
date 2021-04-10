80f0af2c757b67950bbc942689b8e06e7c2854f1
MarioJim <mario.emilio.j@gmail.com>
2021-04-09 19:14:46 -0500

    Display dates on the user page

--- src/main/webapp/user-data.js
+++ src/main/webapp/user-data.js
@@ -20,3 +20,19 @@ function redirectToCalendarPage() {
 function redirectToLocationPage() {
     window.location.replace("choose-loc.html?" + getUserData());
 }
+
+function loadResults() {
+    const params = new URLSearchParams(window.location.search);
+    const event = params.get('event');
+    fetch(`/MatchingAlgoDate?text-input=${event}`)
+        .then(res => res.json())
+        .then(dates => {
+            const dateFormatter = new Intl.DateTimeFormat();
+            const dateElems = dates
+                .map(date => new Date(date))
+                .map(date => dateFormatter.format(date))
+                .map(dateStr => `<li>${dateStr}</li>`);
+            const datesContainer = document.getElementById('dates');
+            datesContainer.innerHTML = dateElems.join('');
+        });
+}
